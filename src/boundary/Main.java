af5383b0a7f185161fd48b6f3720df214e99a375
Daniel Gabbay <d9777834@gmail.com>
2020-04-21 11:29:28 +0300

    reformat code example

--- src/boundary/Main.java
+++ src/boundary/Main.java
@@ -1,5 +1,5 @@
 package boundary;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -10,20 +10,20 @@ import java.time.Clock;
 
 
 public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("loginPageInterface.fxml"));
-			Scene scene = new Scene(root,1000,600);
+			BorderPane root = FXMLLoader.load(getClass().getResource("loginPageInterface.fxml"));
+			Scene scene = new Scene(root, 1000, 600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
