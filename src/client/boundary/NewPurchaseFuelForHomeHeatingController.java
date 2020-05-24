2c7235fdfe5286af44c0ddfded0af4fad2b628de
Daniel Gabbay <d9777834@gmail.com>
2020-05-24 09:32:40 +0300

    הוספת המסכים מהאב טיפוס בממשק - תמונות שלא נטענו יש לטעון שוב לאחר ששמים אותם בתיקיית הmedia

---
+++ src/client/boundary/NewPurchaseFuelForHomeHeatingController.java
@@ -0,0 +1,140 @@
+package client.boundary;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class NewPurchaseFuelForHomeHeatingController {
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnMenus;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    private JFXTextField anotherContactPhoneNumberTXT;
+
+    @FXML
+    private ImageView WrongQuantity1111141;
+
+    @FXML
+    private JFXTextArea noteTXT;
+
+    @FXML
+    private JFXTextField emailAddressTXT;
+
+    @FXML
+    private ImageView WrongQuantity11111211;
+
+    @FXML
+    private JFXTextField fuelQuantityTXT;
+
+    @FXML
+    private ImageView WrongQuantity1111111;
+
+    @FXML
+    private Text incorrectFuelQtyMSG;
+
+    @FXML
+    private ImageView WrongQuantity111112111;
+
+    @FXML
+    private Text incorrectEmailMSG;
+
+    @FXML
+    private JFXTextField customerGenericDetailsTXT;
+
+    @FXML
+    private JFXTextField streetNameTXT;
+
+    @FXML
+    private JFXTextField ApartmentNumberTXT;
+
+    @FXML
+    private JFXTextField cityTXT;
+
+    @FXML
+    private JFXTextField zipCodeTXT;
+
+    @FXML
+    private ImageView WrongQuantity111111;
+
+    @FXML
+    private ImageView WrongQuantity111112;
+
+    @FXML
+    private ImageView WrongQuantity111113;
+
+    @FXML
+    private ImageView WrongQuantity111114;
+
+    @FXML
+    private Text EmptyStreetMSG;
+
+    @FXML
+    private ComboBox<?> shippingMethodComboBOX;
+
+    @FXML
+    private ComboBox<?> dayAndDateComboBox;
+
+    @FXML
+    private JFXButton t17to19BTN;
+
+    @FXML
+    private JFXButton t9to11BTN;
+
+    @FXML
+    private JFXButton t11to13BTN;
+
+    @FXML
+    private JFXButton t13to15BTN;
+
+    @FXML
+    private JFXButton t15to17BTN;
+
+    @FXML
+    private JFXButton t7to9BTN;
+
+    @FXML
+    private Text shippingSummeryDetailsTXT;
+
+    @FXML
+    private Text totalPricesOfAlllInUsdTXT;
+
+    @FXML
+    private Text DeliveryFeeTXT;
+
+    @FXML
+    private Text subTotalPriceInUsdTXT;
+
+    @FXML
+    private Text fuelQuantityInLitersTXT;
+
+    @FXML
+    private Text firstNameTXT;
+
+    @FXML
+    private Text lastNameTXT;
+
+    @FXML
+    private Text scheduledDeliveryDateTXT;
+
+    @FXML
+    private Text deliveryAddressTXT;
+
+}
