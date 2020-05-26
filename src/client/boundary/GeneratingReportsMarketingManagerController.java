24758300750f9797e7bf23ab898088b471fd8695
Nir Asulin <nir_aso@hotmail.com>
2020-05-26 18:54:14 +0300

    הוספת FXML וקונטרולר ל- GeneratingReportsMarketingManager

---
+++ src/client/boundary/GeneratingReportsMarketingManagerController.java
@@ -0,0 +1,8 @@
+package client.boundary;
+
+import javafx.event.ActionEvent;
+
+public class GeneratingReportsMarketingManagerController {
+    public void handleClicks(ActionEvent actionEvent) {
+    }
+}
