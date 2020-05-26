86b9ab12b9d40dfabec89c945d30c166c8525516
hani3 <hani3157>
2020-05-26 12:02:37 +0300

    add controller and logic classes - for sale operation template

---
+++ src/client/boundary/SaleOperationTemplateController.java
@@ -0,0 +1,11 @@
+package client.boundary;
+
+import client.logic.SaleOperationTemplateLogic;
+
+/**
+ * @author hani
+ * @see SaleOperationTemplateLogic - the form's logic class
+ */
+
+public class SaleOperationTemplateController {
+}
