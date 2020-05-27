c9d8a6a955acf98d777c5284408fc5ea4213ccf3
Nir Asulin <nir_aso@hotmail.com>
2020-05-27 15:36:56 +0300

    GeneratingReportsStationManagerController

--- src/client/boundary/GeneratingReportsStationManagerController.java
+++ src/client/boundary/GeneratingReportsStationManagerController.java
@@ -1,8 +1,43 @@
 package client.boundary;
 
+import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class GeneratingReportsStationManagerController {
-    public void handleClicks(ActionEvent actionEvent) {
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    private Button btnGenerateReport;
+
+    @FXML
+    private JFXComboBox<?> ChooseReportToGenerateCombo;
+
+    @FXML
+    void handleChooseReportToGenerate(ActionEvent event) {
+
     }
+
+    @FXML
+    void handleClicks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleGenerateReport(ActionEvent event) {
+
+    }
+
 }
