04e1505e88348dbef9512918df2b2e3c532f318d
Nir Asulin <nir_aso@hotmail.com>
2020-05-27 14:14:31 +0300

    SettingDiscountRatesController

--- src/client/boundary/ConfirmDiscountRatesController.java
+++ src/client/boundary/ConfirmDiscountRatesController.java
@@ -1,8 +1,38 @@
 package client.boundary;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
 public class ConfirmDiscountRatesController {
-    public void handleClicks(ActionEvent actionEvent) {
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    private TableView<?> TableSubscriptionType;
+
+    @FXML
+    private Button btnApprovedRates;
+
+    @FXML
+    void handleApprovedRates(ActionEvent event) {
+
     }
+
+    @FXML
+    void handleClicks(ActionEvent event) {
+
+    }
+
 }
--- src/client/boundary/SettingDiscountRatesController.java
+++ src/client/boundary/SettingDiscountRatesController.java
@@ -1,8 +1,55 @@
 package client.boundary;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SettingDiscountRatesController {
-    public void handleClicks(ActionEvent actionEvent) {
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    private JFXComboBox<?> ChooseSubscriptionTypeCombo;
+
+    @FXML
+    private JFXTextField ShowCurrentRateTXT;
+
+    @FXML
+    private Button btnSetNewRate;
+
+    @FXML
+    private JFXTextField ShowCurrentRateTXT1;
+
+    @FXML
+    void handleChoseSubscriptionType(ActionEvent event) {
+
     }
+
+    @FXML
+    void handleClicks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleSetNewRate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleShowCurrentRate(ActionEvent event) {
+
+    }
+
 }
