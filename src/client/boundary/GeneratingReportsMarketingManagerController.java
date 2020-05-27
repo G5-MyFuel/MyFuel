5ac2744d176304d8b7de056b919f8dcb4a4f17da
Nir Asulin <nir_aso@hotmail.com>
2020-05-27 15:32:57 +0300

    GeneratingReportsMarketingManagerController

--- src/client/boundary/GeneratingReportsMarketingManagerController.java
+++ src/client/boundary/GeneratingReportsMarketingManagerController.java
@@ -1,8 +1,47 @@
 package client.boundary;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class GeneratingReportsMarketingManagerController {
-    public void handleClicks(ActionEvent actionEvent) {
+
+    @FXML
+    private Button btnOverview;
+
+    @FXML
+    private Button btnCustomers;
+
+    @FXML
+    private Button btnPackages;
+
+    @FXML
+    private Button btnSignout;
+
+    @FXML
+    private Button btnGenerateReport;
+
+    @FXML
+    private JFXComboBox<?> ChooseReportToGenerateCombo;
+
+    @FXML
+    private JFXTextField EnterOperationSaleTXT;
+
+    @FXML
+    void handleChooseReportToGenerate(ActionEvent event) {
+
     }
+
+    @FXML
+    void handleClicks(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleEnterOperationSale(ActionEvent event) {
+
+    }
+
 }
