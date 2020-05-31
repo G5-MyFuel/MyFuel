85a035e23a4f8a666a50707c4e1b577c89e5a583
Nir Asulin <nir_aso@hotmail.com>
2020-05-31 19:01:59 +0300

    SettingDiscountRates

--- src/client/boundary/SettingDiscountRatesController.java
+++ src/client/boundary/SettingDiscountRatesController.java
@@ -5,6 +5,8 @@ import client.logic.NewPurchaseFuelForHomeHeatingLogic;
 import client.logic.SettingDiscountRatesLogic;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -42,18 +44,34 @@ public class SettingDiscountRatesController implements Initializable {
     @FXML
     private JFXTextField ShowNewRateTXT;
 
+    private ObservableList<String> SubscriptionType = FXCollections.observableArrayList("Regular monthly subscription - single vehicle",
+            "Full monthly subscription (for single vehicle)", "Regular monthly subscription - number of vehicles");
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.settingDiscountRatesLogic = SettingDiscountRatesLogic.getInstance();
         //this.formValidation = FormValidation.getValidator();
+        ChooseSubscriptionTypeCombo.setItems(SubscriptionType);
         ShowCurrentRateTXT.setVisible(false);
         ShowNewRateTXT.setVisible(false);
+        btnSetNewRate.setVisible(false);
         //ChooseSubscriptionTypeCombo.set
         //this.shippingIndicatorTAB1.setVisible(false);
         /*  set all fields validators */
         //formValidation();   //
         /* set form items */
         //setShippingTab();
+    }
+
+    /**
+     * NewPurchaseFuelForHomeHeatingController Instance getter using SingleTone DesignPatterns
+     * @return Instance of controller class
+     */
+    public SettingDiscountRatesController getInstance() {
+        if (Instance == null)
+            Instance = new SettingDiscountRatesController();
+        return Instance;
     }
 
     /*private void formValidation() {
@@ -64,6 +82,11 @@ public class SettingDiscountRatesController implements Initializable {
     @FXML
     void handleChoseSubscriptionType(ActionEvent event) {
 
+        /*צריך להשוות את המנוי הנבחר עם המידע ב-DB ולהציג את המחיר הקיים.todo:*/
+        ShowCurrentRateTXT.setVisible(true);
+        ShowCurrentRateTXT.setEditable(false);
+        ShowNewRateTXT.setVisible(true);
+        btnSetNewRate.setVisible(true);
     }
 
     @FXML
@@ -79,10 +102,15 @@ public class SettingDiscountRatesController implements Initializable {
     @FXML
     void handleShowCurrentRate(ActionEvent event) {
 
+        /* מחזיר מה-DB את המחיר הנוכחי של התוכנית המתאימה.todo:*/
+
     }
 
     @FXML
     void handleShowNewRate(ActionEvent event) {
+
+        /* מעדכן את ה-DB במחיר החדש של התוכנית המתאימה + בדיקת תקינות.todo:*/
+
 
     }
 
