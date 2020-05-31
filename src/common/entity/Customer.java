2c35a338f24bd2251b2051fa8ddae7560a0268f6
itay_ <itayziv8@gmail.com>
2020-05-31 14:06:22 +0300

    working on CostumerRegistarationPage -> SomeChangeds in Costumer class in entity , all other changes is in costumers page for now.

--- src/common/entity/Customer.java
+++ src/common/entity/Customer.java
@@ -3,7 +3,10 @@ package common.entity;
 public class Customer {
 
     private String ID;
-
+    private String Fname;
+    private String Lname;
+    private String emailAdress;
+    private CreditCard CustomerCreditCard;
 
 
 
