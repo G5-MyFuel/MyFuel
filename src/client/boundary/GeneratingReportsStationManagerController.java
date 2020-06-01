9aea38ee7b86475d5add2ec5bf137dca3694d3f6
Nir Asulin <nir_aso@hotmail.com>
2020-06-01 22:41:21 +0300

    GeneratingReportsStationManagerController GeneratingReportsStationManagerLogic MainForTestsNirGeneratingReportsStationManager

--- src/client/boundary/GeneratingReportsStationManagerController.java
+++ src/client/boundary/GeneratingReportsStationManagerController.java
@@ -1,11 +1,21 @@
 package client.boundary;
 
+import client.logic.ConfirmDiscountRatesLogic;
+import client.logic.FormValidation;
+import client.logic.GeneratingReportsStationManagerLogic;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class GeneratingReportsStationManagerController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GeneratingReportsStationManagerController implements Initializable {
+    private static GeneratingReportsStationManagerController Instance = null;
+    private GeneratingReportsStationManagerLogic generatingReportsStationManagerLogic;
+    private FormValidation formValidation;
 
     @FXML
     private Button btnOverview;
@@ -24,6 +34,40 @@ public class GeneratingReportsStationManagerController {
 
     @FXML
     private JFXComboBox<?> ChooseReportToGenerateCombo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.generatingReportsStationManagerLogic = GeneratingReportsStationManagerLogic.getInstance();
+        //this.formValidation = FormValidation.getValidator();
+
+        btnGenerateReport.setDisable(true);
+        /*ChooseSubscriptionTypeCombo.setItems(SubscriptionType);
+        ShowCurrentRateTXT.setVisible(false);
+        ShowNewRateTXT.setVisible(false);
+        btnSetNewRate.setVisible(false);
+        //ChooseSubscriptionTypeCombo.set
+        //this.shippingIndicatorTAB1.setVisible(false);
+        /*  set all fields validators */
+        //formValidation();   //
+        /* set form items */
+        //setShippingTab();
+    }
+
+    /**
+     * GeneratingReportsStationManagerController Instance getter using SingleTone DesignPatterns
+     *
+     * @return Instance of controller class
+     */
+    public static GeneratingReportsStationManagerController getInstance() {
+        if (Instance == null)
+            Instance = new GeneratingReportsStationManagerController();
+        return Instance;
+    }
+
+    /*private void formValidation() {
+
+
+    }*/
 
     @FXML
     void handleChooseReportToGenerate(ActionEvent event) {
