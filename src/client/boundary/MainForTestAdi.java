390db30ad1e192e8f6ab03bc41ff6d3cf7a99aa9
adidu <adilam995@gmail.com>
2020-06-05 13:46:49 +0300

    adi main

--- src/client/boundary/MainForTestAdi.java
+++ src/client/boundary/MainForTestAdi.java
@@ -1,20 +1,45 @@
 package client.boundary;
 
+import client.Client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MainForTestAdi extends Application {
     //קובץ בדיקות עבור SaleOperationTemplateController
     String pageName = "supplier-OrderExecution-adi.fxml";   //שם קובץ הfxml
+    final public static int DEFAULT_PORT = 5555;
+    public static Client client;
+    public static String server_ip = "";
+    public static String server_port = "";
 
     private AnchorPane root;
     private OrderExecutionController OrderExecutionController;    //אינסטנס של מחלקת הboundary
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static boolean startClient() throws IOException {
+        String host = "";
+        int port = 0; // The port number
+        if (server_port == "")
+            port = DEFAULT_PORT;
+        else
+            port = Integer.parseInt(server_port);
+
+        host = server_ip;
+
+
+        client = new Client(host, port);
+        System.out.println("Client setup connection! " + host + ": " + port);
+
+        return true;
+
     }
 
     @Override
