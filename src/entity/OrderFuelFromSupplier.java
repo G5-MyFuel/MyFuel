bec3ed54057c15c35777ec4ab2cc8685a36d2a66
adidu <adilam995@gmail.com>
2020-06-08 22:36:09 +0300

    adi commit

--- src/entity/OrderFuelFromSupplier.java
+++ src/entity/OrderFuelFromSupplier.java
@@ -31,39 +31,31 @@ public class OrderFuelFromSupplier {
     public Integer getQuantity() {
         return Quantity;
     }
-
-    public void setOrderNumber(String orderNumber) {
-        OrderNumber = orderNumber;
-    }
-
-    public void setFuelType(String companyName) {
-        FuelType = companyName;
-    }
-
-    public void setStationManagerName(String stationManagerName) {
-        StationManagerName = stationManagerName;
-    }
-
-    public void setStationNum(int stationNum) {
-        StationNum = stationNum;
-    }
-
-    public void setQuantity(int quantity) {
-        Quantity = quantity;
-    }
-
     public Date getOrderDate() {
         return OrderDate;
     }
-
-    public void setOrderDate(Date orderDate) {
-        OrderDate = orderDate;
-    }
-
     public String getOrderStatus() {
         return OrderStatus;
     }
 
+    public void setOrderNumber(String orderNumber) {
+        OrderNumber = orderNumber;
+    }
+    public void setFuelType(String companyName) {
+        FuelType = companyName;
+    }
+    public void setStationManagerName(String stationManagerName) {
+        StationManagerName = stationManagerName;
+    }
+    public void setStationNum(int stationNum) {
+        StationNum = stationNum;
+    }
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+    public void setOrderDate(Date orderDate) {
+        OrderDate = orderDate;
+    }
     public void setOrderStatus(String orderStatus) {
         OrderStatus = orderStatus;
     }
