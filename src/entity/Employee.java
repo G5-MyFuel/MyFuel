a1c13e8b9dfe6e3df9fa9841cb6fe3d8a1c9f54e
Daniel Gabbay <d9777834@gmail.com>
2020-06-11 19:54:31 +0300

    עדכון: - שדה userID מטיפוס String ולא int כמו שהיה
    - מפתח לuser הוא גם userID וגם userType

--- src/entity/Employee.java
+++ src/entity/Employee.java
@@ -4,7 +4,7 @@ package entity;
 public class Employee extends User {
 
 
-    public Employee(int userID, String userPassword, String userFirstName, String userLastName, String userEmail) {
+    public Employee(String userID, String userPassword, String userFirstName, String userLastName, String userEmail) {
         super(userID, 2, userPassword, userFirstName, userLastName, userEmail,-1);
     }
 
