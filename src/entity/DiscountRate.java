e4a9126a5f997396eeca3b14e52f706e3aec4802
Nir Asulin <nir_aso@hotmail.com>
2020-06-17 00:02:17 +0300

    ConfirmDiscountRates

---
+++ src/entity/DiscountRate.java
@@ -0,0 +1,42 @@
+package entity;
+
+public class DiscountRate {
+
+    private String subscriptionType;
+    private String currentDiscountRate;
+    private String newDiscountRate;
+
+    public DiscountRate(){
+
+    }
+
+    public DiscountRate(String subscriptionType, String currentDiscountRate, String newDiscountRate) {
+        this.subscriptionType = subscriptionType;
+        this.currentDiscountRate = currentDiscountRate;
+        this.newDiscountRate = newDiscountRate;
+    }
+
+    public String getSubscriptionType() {
+        return subscriptionType;
+    }
+
+    public void setSubscriptionType(String subscriptionType) {
+        this.subscriptionType = subscriptionType;
+    }
+
+    public String getCurrentDiscountRate() {
+        return currentDiscountRate;
+    }
+
+    public void setCurrentDiscountRate(String currentDiscountRate) {
+        this.currentDiscountRate = currentDiscountRate;
+    }
+
+    public String getNewDiscountRate() {
+        return newDiscountRate;
+    }
+
+    public void setNewDiscountRate(String newDiscountRate) {
+        this.newDiscountRate = newDiscountRate;
+    }
+}
