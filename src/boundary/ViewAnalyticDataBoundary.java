c5d6d89688d8b3337562e0838a4e9adbfd09d4ee
hani3 <hani3157>
2020-06-18 14:44:06 +0300

    hani ViewAnalyticData chart

--- src/boundary/ViewAnalyticDataBoundary.java
+++ src/boundary/ViewAnalyticDataBoundary.java
@@ -28,18 +28,21 @@ import java.util.ResourceBundle;
 
 
 /**
- * @author Hana Wiener
+ *  This class This department is responsible for controlling "ViewAnalyticDataFXML" page
+ *  This page display customer ratings (analytic data) based on three different parameters
+ *  And, if necessary, allows data re-creation
+ *
+ *  * @author Hana Wiener
  * @see ViewAnalyticDataController - the form's logic class
  */
-
 public class ViewAnalyticDataBoundary implements DataInitializable {
     /** The supervisor boundary controller. */
     private ViewAnalyticDataController myController = new ViewAnalyticDataController(this);
-    //private AnalyticDataCreator analyticDataCreator = new AnalyticDataCreator(this);
     private generalDashBoardBoundary myDashBoard;
     private String marketingManager;
-    private FormValidation formValidation;
-
+    /**
+     * gui variables
+     */
     @FXML
     private Button btnGenerateAnalyticData;
 
@@ -144,16 +147,31 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
     @FXML
     private Text selectedFuelTypeTXT;
 
-
+    /*
+      Initialize ObservableList in order to display does strings
+      with combo box object.
+       * * */
     private ObservableList<String> CustomerType = FXCollections.observableArrayList("private" , "Company");
     private ObservableList<String> FuelType = FXCollections.observableArrayList("Gasoline95", "Diesel", "ScooterFuel", "HomeHeatingFuel");
 
+    /**
+     *  This method allows to save information sent when uploading the page
+     *
+     * @param data - The data sent to the boundary
+     */
     @Override
     public void initData(Object data) {
          myDashBoard = (generalDashBoardBoundary) data;
 
     }
 
+    /**
+     * This method initializes the variables, fields, and combo-boxes
+     * What is initialized will appear when the screen is raised
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         paneByCustomerType.setVisible(false);
@@ -162,18 +180,27 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         customerTypeCombo.setItems(CustomerType);
         fuelTypeCombo1.setItems(FuelType);
         btnCustomerTypeShow.setDisable(true);
-
     }
 
-    private void formValidation() {
-    }
-
+    /**
+     * This method will only be activated if the "Generate Analitic Data" button is pressed
+     * The method calls for a query that will get customer and purchases data ,
+     *  calculate their ratings, and save to DB.
+     *
+     * @param event
+     */
     @FXML
     void handleGenerateData(ActionEvent event) {
         myController.deletePreviosData();
         myController.getCustomerXPurchaseTable();
     }
 
+    /**
+     *  This method will only be activated if the "Segmentation by fuel type" button is pressed
+     *  The method will display the window that includes fuel type rating.
+     *
+     * @param event
+     */
     @FXML
     void handleBtnFuleType(ActionEvent event) {
         paneByCustomerType.setVisible(false);
@@ -181,6 +208,12 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         paneByFuelType.setVisible(true);
     }
 
+    /**
+     * This method will only be activated if the "Segmentation by fuel type" button is pressed
+     * The method will display the window that includes fuel type rating.
+     *
+     * @param event
+     */
     @FXML
     void handleBtnFuleTypeImage(MouseEvent event) {
         paneByCustomerType.setVisible(false);
@@ -188,6 +221,12 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         paneByFuelType.setVisible(true);
     }
 
+    /**
+     *This method will only be activated if the "Segmentation by refueling hours" button is pressed
+     * The method will display the window that includes hourly rankings
+     *
+     * @param event
+     */
     @FXML
     void handleBtnRefuelHour(ActionEvent event) {
         paneByFuelType.setVisible(false);
@@ -195,6 +234,12 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         paneByTime.setVisible(true);
     }
 
+    /**
+     *This method will only be activated if the "Segmentation by refueling hours" button is pressed
+     * The method will display the window that includes hourly rankings
+     *
+     * @param event
+     */
     @FXML
     void handleBtnRefuelHourImage(MouseEvent event) {
         paneByFuelType.setVisible(false);
@@ -202,6 +247,12 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         paneByTime.setVisible(true);
     }
 
+    /**
+     *This method will only be activated if the "Select Fuel Type:" combo is pressed
+     * The method will not allow clicking "Display Data" until a fuel type is selected
+     *
+     * @param event
+     */
     @FXML
     void handleFuelTypeCombo(ActionEvent event) {
         btnFuelTypeShow.setDisable(false);
@@ -209,16 +260,12 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
 
     }
 
-    public void handleFuelTypeComboShow(ActionEvent actionEvent) {
-        String str = fuelTypeCombo1.getValue();
-        myController.getRatingForFuelTypeTable(str);
-        RatingTableForFuelType.setVisible(true);
-    }
-
-    @FXML
-    void handleTimeRangeCombo(ActionEvent event) {
-    }
-
+    /**
+     *  This method will only be activated if the "Select Customer Type:" combo is pressed
+     *  The method will not allow clicking "Display Data" until a Customer type is selected
+     *
+     * @param event
+     */
     @FXML
     void handleCustomerTypeComboClick(ActionEvent event) {
         btnCustomerTypeShow.setDisable(false);
@@ -226,6 +273,38 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
 
     }
 
+    /**
+     *This method will only be activated if the "Show Segmentation by customer type" button is pressed
+     * The method will display the window that show ranking by customer type parameter
+     *
+     * @param event
+     */
+    @FXML
+    void handleBtnCustomerType(ActionEvent event) {
+        paneByFuelType.setVisible(false);
+        paneByTime.setVisible(false);
+        paneByCustomerType.setVisible(true);
+
+    }
+
+    /**
+     *  This method will only be activated if the "Show Segmentation by fuel type" button is pressed
+     *  The method will begin the process of sending a query that will bring DB to the ratings table
+     *
+     * @param actionEvent
+     */
+    public void handleFuelTypeComboShow(ActionEvent actionEvent) {
+        String str = fuelTypeCombo1.getValue();
+        myController.getRatingForFuelTypeTable(str);
+        RatingTableForFuelType.setVisible(true);
+    }
+
+    /**
+     *  This method will only be activated if the "Show Segmentation by customer type" button is pressed
+     *  The method will begin the process of sending a query that will bring DB to the ratings table
+     *
+     * @param event
+     */
     @FXML
     void handlecustomerTypeCombo(ActionEvent event) {
         String str = customerTypeCombo.getValue();
@@ -234,7 +313,34 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
 
     }
 
+    /**
+     *  This method will only be activated if the "Segmentation by refueling hours" button is pressed
+     *  The method will begin the process of sending a query that will bring DB to the ratings table
+     *
+     *  * @param actionEvent
+     */
+    public void handleTimePickerShow(ActionEvent actionEvent) {
+        myController.getRatingForTimeRangeTable(Time.valueOf(TimeRangeStart.getValue()),Time.valueOf(TimeRangeEnd.getValue()));
 
+    }
+
+    //TODO: continue java doc
+    /**
+     *
+     * @param event
+     */
+    @FXML
+    void handleBtnCustomerTypeImage(MouseEvent event) {
+        paneByFuelType.setVisible(false);
+        paneByTime.setVisible(false);
+        paneByCustomerType.setVisible(true);
+
+    }
+
+    /**
+     *
+     * @param cosArray
+     */
     public void setRatingForCustomerTypeTable(ArrayList<Rating> cosArray){
         ratingCulomn1.setCellValueFactory(new PropertyValueFactory<>("rating"));
         customerIdCulomn1.setCellValueFactory(new PropertyValueFactory<>("customerID"));
@@ -245,8 +351,10 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         RatingTableForCustomerType.setItems(data);
     }
 
-
-
+    /**
+     *
+     * @param cosArray
+     */
     public void setRatingForFuelTypeTable(ArrayList<Rating> cosArray){
         ratingCulomn11.setCellValueFactory(new PropertyValueFactory<>("rating"));
         customerIdCulomn11.setCellValueFactory(new PropertyValueFactory<>("customerID"));
@@ -257,7 +365,10 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         RatingTableForFuelType.setItems(data);
     }
 
-
+    /**
+     *
+     * @param cosArray
+     */
     public void setRatingForTimeRangeTable(ArrayList<Rating> cosArray){
         ratingCulomn.setCellValueFactory(new PropertyValueFactory<>("rating"));
         customerIdCulomn.setCellValueFactory(new PropertyValueFactory<>("customerID"));
@@ -268,29 +379,13 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         RatingTableForTimeRange.setItems(data);
     }
 
-    public void handleTimePickerShow(ActionEvent actionEvent) {
-         myController.getRatingForTimeRangeTable(Time.valueOf(TimeRangeStart.getValue()),Time.valueOf(TimeRangeEnd.getValue()));
 
-    }
-
-
-
-    @FXML
-    void handleBtnCustomerType(ActionEvent event) {
-        paneByFuelType.setVisible(false);
-        paneByTime.setVisible(false);
-        paneByCustomerType.setVisible(true);
-
-    }
-
-    @FXML
-    void handleBtnCustomerTypeImage(MouseEvent event) {
-        paneByFuelType.setVisible(false);
-        paneByTime.setVisible(false);
-        paneByCustomerType.setVisible(true);
-
-    }
 //pai chart methodes :
+
+    /**
+     *
+     * @param data
+     */
     private void showPieChartCustomerType(ObservableList<Rating> data) {
         int[] monim = new int[11];
         for(int i=0;i<10;i++)
@@ -307,6 +402,10 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
         pieChartCustomerType.setData(piedata);
     }
 
+    /**
+     *
+     * @param data
+     */
     private void showPieChartFuelType(ObservableList<Rating> data) {
         int[] monim = myController.monim;
         ObservableList<PieChart.Data> piedata1 = FXCollections.observableArrayList(
@@ -319,6 +418,10 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
 
     }
 
+    /**
+     *
+     * @param data
+     */
     private void showPieChartHouers(ObservableList<Rating> data) {
         int[] monim = myController.monim;
         ObservableList<PieChart.Data> piedata2 = FXCollections.observableArrayList(
@@ -331,4 +434,7 @@ public class ViewAnalyticDataBoundary implements DataInitializable {
 
     }
 
+    @FXML
+    void handleTimeRangeCombo(ActionEvent event) {
+    }
 }
