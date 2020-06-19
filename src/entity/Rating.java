4f0050beb3bf82f13ce1b68793823447ac9d1df2
hani3 <hani3157>
2020-06-19 13:22:22 +0300

    hani java doc entity

--- src/entity/Rating.java
+++ src/entity/Rating.java
@@ -11,7 +11,13 @@ public class Rating {
     private String customerID;
     private String customerType;
 
-
+    /**
+     * Constructor
+     *
+     * @param rating
+     * @param customerID
+     * @param customerType
+     */
     public Rating(Integer rating, String customerID, String customerType){
         this.rating = rating;
         this.customerID = customerID;
@@ -19,26 +25,50 @@ public class Rating {
 
     }
 
+    /**
+     *  get  Rating method
+     * @return rating
+     */
     public Integer getRating() {
         return rating;
     }
 
+    /**
+     * set Rating method
+     * @param rating
+     */
     public void setRating(int rating) {
         this.rating = rating;
     }
 
+    /**
+     *  get  CustomerID method
+     * @return customerID
+     */
     public String getCustomerID() {
         return customerID;
     }
 
+    /**
+     * set CustomerID method
+     * @param customerID
+     */
     public void setCustomerID(String customerID) {
         this.customerID = customerID;
     }
 
+    /**
+     * get  customerType method
+     * @return customerType
+     */
     public String getCustomerType() {
         return customerType;
     }
 
+    /**
+     * set customerType method
+     * @param customerType
+     */
     public void setCustomerType(String customerType) {
         this.customerType = customerType;
     }
